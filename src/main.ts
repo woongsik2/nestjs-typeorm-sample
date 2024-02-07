55b10b37ef77eea9d5d6cd55a0fbebbf06289bc2
woongsik <w.dev.bear@gmail.com>
2024-02-07 17:01:13 +0900

    ✨Swagger 셋팅 및 필요 모듈 추가.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const config = new DocumentBuilder().setTitle('API TEST').setDescription('REST API Test Sample').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3333);
 }
 bootstrap();
